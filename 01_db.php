54560d17a985b278addb181186a35b4b6dace2eb
Foxh <kymhbonyh79@gmail.com>
2023-03-15 18:41:24 +0300

    first commit

---
+++ 01_db.php
@@ -0,0 +1,27 @@
+<?php
+
+//connection string constants
+define('_HOST', 'localhost');
+define('_USERNAME', 'root');
+define('_PASS', '');
+define('DB_NAME', 'Maseno');
+
+//connect to and select database of focus
+$conn = new mysqli(_HOST, _USERNAME, _PASS, DB_NAME);
+
+//check connection
+if ($conn->connect_error) {
+    die('Connection failed' . $conn->connect_error);
+}
+echo 'CONNECTED';
+
+//create table
+mysqli_query($conn, "CREATE TABLE STUDENT(name varchar(255), regNo varchar(255), age int(255),
+     primary key(regNo)");
+
+//insert data into the table
+mysqli_query($conn, "INSERT INTO STUDENT(name, regNo, age)
+     VALUES('Boniface Mwau','CI/00167/017', 25)");
+
+//close database engine
+mysqli_close($conn);
---
+++ form.php
@@ -0,0 +1,22 @@
+<?php include '01_db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submission Form</title>
+</head>
+
+<body>
+    <div class="header" id="header">
+        <a href="#">Foxh</a>
+        <a href="#">Home</a>
+        <a href="#">Feedback</a>
+        <a href="#">About</a>
+    </div>
+</body>
+
+</html>
